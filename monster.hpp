6275f02a8313c39d30571e9f2cd94bae413b3a4c
CriticalDMG <57966930+CriticalDMG666@users.noreply.github.com>
2021-03-02 16:11:07 +0200

    .hpp files
    .hpp files for the monsters and the player. just testing for bobi

---
+++ monster.hpp
@@ -0,0 +1,17 @@
+#ifndef MONSTER_HPP
+#define MONSTER_HPP
+
+#include <iostream.h>
+using namespace std;
+
+int mMaxHP;
+int mCurrHP;
+int mBaseDMG;
+int mFinDMG;
+String mName;
+String mDesc;
+
+void mAttack(){
+}
+
+#endif
