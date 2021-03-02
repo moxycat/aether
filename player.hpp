2656ab2555b02e0fc4b86a61df0d82185684d414
CriticalDMG <57966930+CriticalDMG666@users.noreply.github.com>
2021-03-02 16:35:28 +0200

    Add files via upload

---
+++ player.hpp
@@ -0,0 +1,25 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <iostream.h>
+using namespace std;
+
+int pMaxHP;
+int pCurrHP;
+int pBaseDMG;
+int pFinDMG;
+int pDMGRand;
+int pEsc;
+bool pEscSuc;
+String pName;
+
+void pAttack(){
+}
+
+void pDefend(){
+}
+
+void pRun(){
+}
+
+#endif
