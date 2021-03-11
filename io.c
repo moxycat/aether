768482b3087563da45ed0fae01fae57bd097fa09
CriticalDMG <57966930+CriticalDMG666@users.noreply.github.com>
2021-03-11 10:34:51 +0200

    Add files via upload

---
+++ io.c
@@ -0,0 +1,25 @@
+#include "io.h"
+
+void console_clear() {
+    COORD topLeft  = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
+}
+
+void slowprint(char *str) {
+    for (int i = 0; i < strlen(str); ++i) {
+        putchar(str[i]);
+        Sleep(25);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Linear Stage-based RPG
